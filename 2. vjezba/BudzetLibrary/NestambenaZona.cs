a57e9398af49fb862c5fc89add09c9d569126055
Niko <niko.rastija@gmail.com>
2024-11-13 21:56:20 +0100

    Design and implement class NestambenaZona for inserting complex parts that aren't livable

---
+++ 2. vjezba/BudzetLibrary/NestambenaZona.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudzetLibrary
+{
+    public class NestambenaZona : Zona
+    {
+        public NestambenaZona(string oznaka, List<Prostor> prostori) : base(oznaka, prostori)
+        {
+        }
+
+        public override double izracunajPricuvu()
+        {
+            return ukupnaPovrsinaZone() * 3;
+        }
+    }
+}
