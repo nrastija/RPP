d7878cfc10f7963cb359961afee8e7e756473b23
Niko <niko.rastija@gmail.com>
2024-11-13 18:34:21 +0100

    Finished testing console with fixed variables #1

--- 2. vjezba/KonzolaGradevina/Program.cs
+++ 2. vjezba/KonzolaGradevina/Program.cs
@@ -14,10 +14,22 @@ class Program
         gradevina.dodajProstor(new VanjskiProstor("Parking", 120, false));
         gradevina.dodajProstor(new VanjskiProstor("Nadstresnica", 50, true));
 
-        Console.Write("Neto povrsina prostora FOI-ja: " + gradevina.dohvatiVlastituNetoPovrsinu() + " m^2");
+        Console.Write("Unutarnji prostori:\n");
+        foreach (var unutarnjiProstor in gradevina.dohvatiUnutarnjeProstore())
+        {
+            Console.Write("Unutarnji prostor: " + unutarnjiProstor.Oznaka + " - " + unutarnjiProstor.Povrsina + " m^2\n");
+        }
+
+        Console.Write("\nVanjski prostori:\n");
+        foreach (var vanjskiProstor in gradevina.dohvatiVanjskeProstore())
+        {
+            Console.Write("Vanjski: " + vanjskiProstor.Oznaka + " - " + vanjskiProstor.Povrsina + " m^2\n");
+        }
 
 
+        Console.Write("\nNeto povrsina prostora FOI-ja: " + gradevina.dohvatiVlastituNetoPovrsinu() + " m^2");
 
+        
 
 
     }
