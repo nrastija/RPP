b16406cb3f3c85abd16e8d6c5355a38ab6b80bf1
Niko <niko.rastija@gmail.com>
2024-11-13 20:25:44 +0100

    Implement class Viber

---
+++ 2. vjezba/Messenger (Zadatak 5)/MessengerLib/Viber.cs
@@ -0,0 +1,22 @@
+﻿using DataLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessengerLib
+{
+    public class Viber : IKanal
+    {
+        public string posaljiPoruku(User user, string poruka)
+        {
+            if (string.IsNullOrEmpty(user.ViberID))
+            {
+                return $"Viber message was not sent to {user.FirstName} {user.LastName} because no Viber ID was provided.";
+            }
+
+            return $"Viber message sent to {user.ViberID} with message: {poruka}";
+        }
+    }
+}
