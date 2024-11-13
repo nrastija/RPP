f2549734eee92bdc9c4f29ac0038e65b831a9456
Niko <niko.rastija@gmail.com>
2024-11-13 21:46:01 +0100

    Create main project folder and add class Prostor

---
+++ 2. vjezba/BudzetLibrary/Prostor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudzetLibrary
+{
+    public class Prostor
+    {
+        public string Oznaka { get; set; }
+        public double Povrsina { get; set; } 
+
+        public Prostor(string oznaka, double povrsina)
+        {
+            Oznaka = oznaka;
+            Povrsina = povrsina;
+        }
+    }
+}
