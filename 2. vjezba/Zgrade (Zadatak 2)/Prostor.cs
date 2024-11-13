3b2bbe59b8a4f6f6b4d54e03a283b33ca27a2f61
Niko <niko.rastija@gmail.com>
2024-11-13 18:54:21 +0100

    Implemented abstract class Prostor which will be inherited by two new classes implemented in the future

---
+++ 2. vjezba/Zgrade (Zadatak 2)/Prostor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zgrade__Zadatak_2_
+{
+    public abstract class Prostor
+    {
+        public string Oznaka { get; }
+        public int Kat { get; }
+        public float Povrsina { get; }
+        public bool Slobodan { get; set; }
+
+        private int cijena;
+        public int Cijena { 
+            get => cijena;
+            set
+            {
+                if (value == 0)
+                {
+                    throw new Exception("Cijena ne moze biti 0");
+                }
+                Cijena = value;
+            }
+        }
+
+        public Prostor(string oznaka, int kat, float povrsina, bool slobodan, int cijena)
+        {
+            Oznaka = oznaka;
+            Kat = kat;
+            Povrsina = povrsina;
+            Slobodan = slobodan;
+            Cijena = cijena;
+        }
+
+        public abstract double izracunajProdajnuCijenu();
+    }
+}
