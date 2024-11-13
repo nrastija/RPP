184a263bc3800d5fa2075397373d3af1c746fcdb
Niko <niko.rastija@gmail.com>
2024-11-13 21:52:28 +0100

    Implement class Zona

---
+++ 2. vjezba/BudzetLibrary/Zona.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudzetLibrary
+{
+    public abstract class Zona
+    {
+        public string Oznaka { get; set; }  
+        public List<Prostor> popisProstora { get; set; }
+
+        public void dodajProstor(Prostor prostor)
+        {
+            popisProstora.Add(prostor);
+        }
+
+        public double ukupnaPovrsinaZone()
+        {
+            return popisProstora.Sum(p => p.Povrsina);
+        }
+        public abstract double izracunajPricuvu();
+    }
+}
