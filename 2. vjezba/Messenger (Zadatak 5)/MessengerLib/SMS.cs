76ca0c4cd9591caa285483e1bd90c0eb21f0e9ff
Niko <niko.rastija@gmail.com>
2024-11-13 20:27:35 +0100

    Implement class SMS

---
+++ 2. vjezba/Messenger (Zadatak 5)/MessengerLib/SMS.cs
@@ -0,0 +1,22 @@
+﻿using DataLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessengerLib
+{
+    public class SMS : IKanal
+    {
+        public string posaljiPoruku(User user, string poruka)
+        {
+            if (string.IsNullOrEmpty(user.Phone))
+            {
+                return $"SMS not sent to {user.FirstName} {user.LastName} because no phone number was provided.";
+            }
+
+            return $"SMS sent to {user.Phone} with message: {poruka}";
+        }
+    }
+}
