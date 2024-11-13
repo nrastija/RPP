eda994220c6d19d4ad333e9c7df67be7b390cb10
Niko <niko.rastija@gmail.com>
2024-11-13 22:52:13 +0100

    Implemented class Email for sending emails through GUI

---
+++ 2. vjezba/Komunikacijski Kanali (Zadatak 6)/ClassLibrary1/Email.cs
@@ -0,0 +1,30 @@
+﻿using ClassLibrary1;
+using DataLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessengerLib
+{
+    internal class Email : IKanal
+    {
+        public string saljiPoruku(User user, string poruka)
+        {
+            if (!Validacija(user, poruka))
+            {
+                throw new Exception("Message not sent.");
+            }
+            
+            return $"Email sent to {user.Email} with message: {poruka}";
+
+        }
+
+        protected virtual bool Validacija(User user, string poruka)
+        {
+            return user != null && !string.IsNullOrWhiteSpace(poruka);
+        }
+    }
+}
