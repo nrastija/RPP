07f41b1f34c08cea76651872721dd881c1812b9b
Niko <niko.rastija@gmail.com>
2024-11-13 19:01:49 +0100

    Implement class NestambeniProstor for building parts that are not livable

---
+++ 2. vjezba/Zgrade (Zadatak 2)/NestambeniProstor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zgrade__Zadatak_2_
+{
+    public class NestambeniProstor : Prostor
+    {
+        public NestambeniProstor(string oznaka, int kat, float povrsina, bool slobodan, int cijena) : base(oznaka, kat, povrsina, slobodan, cijena)
+        {
+        }
+
+        public override double izracunajProdajnuCijenu()
+        {
+            return Povrsina * Cijena * 1.03;
+        }
+    }
+}
