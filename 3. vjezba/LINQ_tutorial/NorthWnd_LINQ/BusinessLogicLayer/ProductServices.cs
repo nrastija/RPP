39b4a0b5303aa26d4a8759ab0373cc2a266f9102
Niko <niko.rastija@gmail.com>
2024-12-02 19:15:43 +0100

    Implement ProductServices for Business Layer

--- 3. vjezba/LINQ_tutorial/NorthWnd_LINQ/BusinessLogicLayer/ProductServices.cs
+++ 3. vjezba/LINQ_tutorial/NorthWnd_LINQ/BusinessLogicLayer/ProductServices.cs
@@ -3,10 +3,85 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DataAccessLayer.Repositories;
+using EntitiesLayer.Entities;
 
 namespace BusinessLogicLayer
 {
     internal class ProductServices
     {
+        public List<Product> GetProducts()
+        {
+            using (var repo = new ProductRepository())
+            {
+                List<Product> products = repo.GetAll().ToList();
+                return products;
+            }
+        }
+
+        public List<Product> GetProductsByName(string phrase)
+        {
+            using (var repo = new ProductRepository())
+            {
+                List<Product> products = repo.GetProductsByName(phrase).ToList();
+                return products;
+            }
+        }
+
+        public bool AddProduct(Product product)
+        {
+            bool isSuccessful = false;
+
+            using (var repo = new ProductRepository())
+            {
+                int affectedRows = repo.Add(product);
+                isSuccessful = affectedRows > 0;
+            }
+
+            return isSuccessful;
+        }
+
+        public bool UpdateProduct(Product product)
+        {
+            bool isSuccessful = false;
+
+            using (var repo = new ProductRepository())
+            {
+                int affectedRows = repo.Update(product);
+                isSuccessful = affectedRows > 0;
+            }
+
+            return isSuccessful;
+        }
+
+        public bool RemoveProduct(Product product)
+        {
+            bool isSuccessful = false;
+
+            bool canRemove = CheckIfProductCanBeRemoved(product);
+            if (canRemove)
+            {
+                using (var repo = new ProductRepository())
+                {
+                    int affectedRows = repo.Remove(product);
+                    isSuccessful = affectedRows > 0;
+                }
+            }
+
+            return isSuccessful;
+        }
+
+        private bool CheckIfProductCanBeRemoved(Product product)
+        {
+            if (product == null || product.Discontinued == false || product.UnitsInStock > 0 ||
+                product.UnitsOnOrder > 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
     }
 }
