bdca22bd0c012315556cf15fbb91139b0cfd5c9d
nrastija <niko.rastija@gmail.com>
2024-12-03 13:57:25 +0100

    Finished DataAccessLayer logic implementation

---
+++ 3. vjezba/Zadatak3_2/DataAccessLayer/CustomerRepository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public class CustomerRepository : Repository<Customer>
+    {
+        public CustomerRepository() : base(new Northwind())
+        {
+        }
+
+        public IQueryable<Customer> GetCustomerByCountryAndCity(string country, string city)
+        {
+            var query = from c in Entities where c.Country == country && c.City == city select c;
+            return query;
+        }
+
+        public IQueryable<string> GetCountries()
+        {
+            var query = from c in Entities select c.Country;
+            return query;
+        }
+
+        public IQueryable<string> GetCities()
+        {
+            var query = from c in Entities select c.City;
+            return query;
+        }
+    }
+}
