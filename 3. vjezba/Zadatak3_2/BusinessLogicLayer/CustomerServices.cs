0338e6bedaa50553b7680f62d3a30cf6dd6b057a
nrastija <niko.rastija@gmail.com>
2024-12-03 14:50:33 +0100

    Implement BusinessLogicLayer for getting customers by city and country

---
+++ 3. vjezba/Zadatak3_2/BusinessLogicLayer/CustomerServices.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer;
+
+namespace BusinessLogicLayer
+{
+    public class CustomerServices
+    {
+        public List<Customer> GetCustomerByCountryAndCity(string country, string city)
+        {
+            using (var repo = new CustomerRepository())
+            {
+                List<Customer> customers = repo.GetCustomerByCountryAndCity(country, city).ToList();
+                return customers;
+            }
+        }
+    }
+}
