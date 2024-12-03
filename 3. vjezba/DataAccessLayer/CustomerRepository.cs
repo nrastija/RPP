8d7b1b4f611f14e361afa1b777fbef24d5de84e1
nrastija <niko.rastija@gmail.com>
2024-12-03 08:55:29 +0100

    Finish DataAccessLayer implementation logic for showing all customers and filtering them by country

---
+++ 3. vjezba/DataAccessLayer/CustomerRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public class CustomerRepository : Repository<Customer>
+    {
+        public CustomerRepository() : base(new Model1())
+        {
+            
+        }
+
+        public override IQueryable<Customer> GetAll()
+        {
+            var query = from e in Entities select e;
+            return query;
+        }
+
+        public IQueryable<Customer> GetCustomersByCounty(string country)
+        {
+            var query = from e in Entities where e.Country == country select e;
+            return query;
+        }
+    }
+}
