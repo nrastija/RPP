c3c2f65136ef4e71cd9e538659112bde109cb753
nrastija <niko.rastija@gmail.com>
2024-12-03 15:58:19 +0100

    Implement BusinessLogicLayer part for showing customers and adding a new customer

---
+++ 3. vjezba/Zadatak3_3/Zadatak3_3/BusinessLogicLayer/CustomerServices.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer;
+
+namespace BusinessLogicLayer
+{
+    public class CustomerServices
+    {
+        List<Customer> getCustomers()
+        {
+            using (var repo = new CustomerRepository())
+            {
+                List<Customer> customers = repo.GetAll().ToList();
+                return customers;
+            }
+        }
+
+        public bool AddCustomer(Customer customer)
+        {
+            bool isSuccessful = false;
+
+            using (var repo = new CustomerRepository())
+            {
+                int affectedRows = repo.Add(customer);
+                isSuccessful = affectedRows > 0;
+            }
+
+            return isSuccessful;
+        }
+        
+    }
+}
