d631f6d37d189c4d059d64c5dcaedeeb122f896d
nrastija <niko.rastija@gmail.com>
2024-12-04 20:27:02 +0100

    Finish PresentationLayer for using GUI

--- 3. vjezba/Zadatak3_1/Zadatak3_1/CustomersApp/FrmCustomers.Designer.cs
+++ 3. vjezba/Zadatak3_1/Zadatak3_1/CustomersApp/FrmCustomers.Designer.cs
@@ -42,18 +42,21 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.dgvCustomers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvCustomers.Location = new System.Drawing.Point(12, 43);
+            this.dgvCustomers.Location = new System.Drawing.Point(18, 66);
+            this.dgvCustomers.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.dgvCustomers.Name = "dgvCustomers";
-            this.dgvCustomers.Size = new System.Drawing.Size(870, 279);
+            this.dgvCustomers.RowHeadersWidth = 62;
+            this.dgvCustomers.Size = new System.Drawing.Size(1305, 429);
             this.dgvCustomers.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(485, 18);
+            this.label1.Location = new System.Drawing.Point(728, 28);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(85, 13);
+            this.label1.Size = new System.Drawing.Size(127, 20);
             this.label1.TabIndex = 1;
             this.label1.Text = "Filter by Country:";
             // 
@@ -61,43 +64,50 @@
             // 
             this.cmbCountry.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.cmbCountry.FormattingEnabled = true;
-            this.cmbCountry.Location = new System.Drawing.Point(576, 15);
+            this.cmbCountry.Location = new System.Drawing.Point(864, 23);
+            this.cmbCountry.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.cmbCountry.Name = "cmbCountry";
-            this.cmbCountry.Size = new System.Drawing.Size(121, 21);
+            this.cmbCountry.Size = new System.Drawing.Size(180, 28);
             this.cmbCountry.TabIndex = 2;
             // 
             // btnApplyFilter
             // 
             this.btnApplyFilter.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnApplyFilter.Location = new System.Drawing.Point(703, 15);
+            this.btnApplyFilter.Location = new System.Drawing.Point(1054, 23);
+            this.btnApplyFilter.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnApplyFilter.Name = "btnApplyFilter";
-            this.btnApplyFilter.Size = new System.Drawing.Size(85, 23);
+            this.btnApplyFilter.Size = new System.Drawing.Size(128, 35);
             this.btnApplyFilter.TabIndex = 3;
             this.btnApplyFilter.Text = "Apply filter";
             this.btnApplyFilter.UseVisualStyleBackColor = true;
+            this.btnApplyFilter.Click += new System.EventHandler(this.btnApplyFilter_Click);
             // 
             // btnClearFilter
             // 
             this.btnClearFilter.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnClearFilter.Location = new System.Drawing.Point(794, 15);
+            this.btnClearFilter.Location = new System.Drawing.Point(1191, 23);
+            this.btnClearFilter.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnClearFilter.Name = "btnClearFilter";
-            this.btnClearFilter.Size = new System.Drawing.Size(85, 23);
+            this.btnClearFilter.Size = new System.Drawing.Size(128, 35);
             this.btnClearFilter.TabIndex = 4;
             this.btnClearFilter.Text = "Clear filter";
             this.btnClearFilter.UseVisualStyleBackColor = true;
+            this.btnClearFilter.Click += new System.EventHandler(this.btnClearFilter_Click);
             // 
             // FrmCustomers
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(894, 334);
+            this.ClientSize = new System.Drawing.Size(1341, 514);
             this.Controls.Add(this.btnClearFilter);
             this.Controls.Add(this.btnApplyFilter);
             this.Controls.Add(this.cmbCountry);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.dgvCustomers);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "FrmCustomers";
             this.Text = "Customers";
+            this.Load += new System.EventHandler(this.FrmCustomers_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvCustomers)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
--- 3. vjezba/Zadatak3_1/Zadatak3_1/CustomersApp/FrmCustomers.cs
+++ 3. vjezba/Zadatak3_1/Zadatak3_1/CustomersApp/FrmCustomers.cs
@@ -7,14 +7,55 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using BusinessLogicLayer;
+using DataAccesLayer;
 
 namespace CustomersApp
 {
     public partial class FrmCustomers : Form
     {
+        private CustomerServices services = new CustomerServices();
         public FrmCustomers()
         {
             InitializeComponent();
+        }
+
+        private void FrmCustomers_Load(object sender, EventArgs e)
+        {
+            GetCountriesForComboBox();
+            GetAllCustomers();
+        }
+
+        private void GetAllCustomers()
+        {
+            List<Customer> customers = services.GetAllCustomers();
+            dgvCustomers.DataSource = customers;
+        }
+
+        private void GetCountriesForComboBox()
+        {
+            List<string> countries = services.GetCountries();
+            cmbCountry.DataSource = countries;
+        }
+
+        private void btnClearFilter_Click(object sender, EventArgs e)
+        {
+            GetAllCustomers();
+        }
+
+        private void btnApplyFilter_Click(object sender, EventArgs e)
+        {
+            string filter = cmbCountry.Text;
+            if (filter == "")
+            {
+                MessageBox.Show("Potrebno je izabrati državu");
+                return;
+            }
+            else
+            {
+                List<Customer> filtered = services.GetCustomersFromCountry(filter);
+                dgvCustomers.DataSource = filtered;
+            }
         }
     }
 }
