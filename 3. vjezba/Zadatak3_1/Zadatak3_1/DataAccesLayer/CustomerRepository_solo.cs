8df485e87e0be6ed0d66e24f52918ed038e6ec07
nrastija <niko.rastija@gmail.com>
2024-12-04 20:16:57 +0100

    Implement class CustomerRepository that does not inherit class Repository (type 2)

---
+++ 3. vjezba/Zadatak3_1/Zadatak3_1/DataAccesLayer/CustomerRepository_solo.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccesLayer;
+
+namespace DataAccessLayer
+{
+    public class CustomerRepository_solo : IDisposable
+    {
+        private Model1 Context { get; set; }
+
+        public DbSet<Customer> Entities { get; set; }
+
+        public CustomerRepository_solo()
+        {
+            Context = new Model1();
+            Entities = Context.Set<Customer>();
+        }
+
+        public IQueryable<Customer> ShowAllCustomers()
+        {
+            var query = from c in Entities select c;
+            return query;
+        }
+
+        public IQueryable<Customer> FilterCustomersByCounty(string country)
+        {
+            var query = from c in Entities where c.Country == country select c;
+            return query;
+        }
+
+        public IQueryable<string> GetAllCountries()
+        {
+            var query = (from c in Entities select c.Country).Distinct();
+            return query;
+        }
+
+        public void Dispose()
+        {
+            Context.Dispose();
+        }
+    }
+}
