6d16f1421626fa801de805f6f0c7b7868539c577
nrastija <niko.rastija@gmail.com>
2024-12-19 08:49:15 +0100

    Finish implementation for adding products via form

--- 4. vjezba/vj4_zadatak6/vj4_zadatak6/NewProduct.xaml.cs
+++ 4. vjezba/vj4_zadatak6/vj4_zadatak6/NewProduct.xaml.cs
@@ -20,13 +20,59 @@ namespace vj4_zadatak6
     /// </summary>
     public partial class NewProduct : UserControl
     {
+        private NorthwndModel db = new NorthwndModel();
         public NewProduct()
         {
             InitializeComponent();
+            LoadCategories();
+            LoadSuppliers();
+        }
+
+        private void LoadSuppliers()
+        {
+            var suppliers = db.Suppliers.ToList();
+            CmbSupplier.ItemsSource = suppliers;
+            CmbSupplier.DisplayMemberPath = "CompanyName";
+            CmbSupplier.SelectedValuePath = "SupplierID";
+        }
+
+        private void LoadCategories()
+        {
+            var categories = db.Categories.ToList();
+            CmbCategory.ItemsSource = categories;
+            CmbCategory.DisplayMemberPath = "CategoryName";
+            CmbCategory.SelectedValuePath = "CategoryID";
         }
 
         private void BtnCancel_Click(object sender, RoutedEventArgs e)
         {
+            var window = Window.GetWindow(this) as MainWindow;
+
+            if (window != null)
+            {
+                window.ControlPanel.Content = new ShowProducts();
+            }
+        }
+
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+            var newProduct = new Product
+            {
+                ProductName = TxtProductName.Text,
+                Category = CmbCategory.SelectedItem as Category,
+                Supplier = CmbSupplier.SelectedItem as Supplier
+            };
+
+            if (newProduct.ProductName == null || newProduct.Category == null ||
+                newProduct.Supplier == null)
+            {
+                MessageBox.Show("Potrebno je unesti sva polja.");
+                return;
+            }
+
+            db.Products.Add(newProduct);
+            db.SaveChanges();
+
             var window = Window.GetWindow(this) as MainWindow;
 
             if (window != null)
