965657c904f19bb4733c2714413ebf891e8eac53
nrastija <niko.rastija@gmail.com>
2024-12-19 08:26:07 +0100

    Design MainWindow nav bar

--- 4. vjezba/vj4_zadatak6/vj4_zadatak6/MainWindow.xaml.cs
+++ 4. vjezba/vj4_zadatak6/vj4_zadatak6/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace vj4_zadatak6
         {
             InitializeComponent();
         }
+
+        private void BtnExit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
     }
 }
