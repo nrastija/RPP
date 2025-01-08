59cff2c95b10d260efc00be0d924b5aac8da2172
nrastija <niko.rastija@gmail.com>
2025-01-08 18:39:01 +0100

    Implement unit test for testing type of premium client

---
+++ 5. vjezba/Zadatak_2/Tests/UnitTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Entities.Clients;
+
+namespace Tests
+{
+    [TestClass]
+    public class UnitTests
+    {
+        [TestMethod]
+        public void PremiumClient_Type_EqualsClientTypePremium()
+        {
+            //Arrange
+            PremiumClient client = new PremiumClient("1111", "temp", "test", "test");
+
+            //Assert
+            Assert.IsTrue(client.Type == ClientType.Premium);
+            
+        }
+
+        
+    }
+}
