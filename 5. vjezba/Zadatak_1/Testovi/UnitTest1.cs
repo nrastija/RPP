fc6a4596d7054c89bec968f4a212b093c9fc60aa
nrastija <niko.rastija@gmail.com>
2025-01-08 17:29:03 +0100

    Create unit test for testing ClientException error class

--- 5. vjezba/Zadatak_1/Testovi/UnitTest1.cs
+++ 5. vjezba/Zadatak_1/Testovi/UnitTest1.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Runtime.InteropServices;
+using BusinessLayer;
+using BusinessLayer.Exceptions;
 using Entities.Accounts;
 using Entities.Clients;
 using Entities.Exchange;
@@ -35,5 +37,27 @@ namespace Testovi
             Assert.AreEqual(1250, account.Balance, "Nije dobar iznos");
         }
 
+        [TestMethod]
+        public void ClientService_UpdatePremiumClient_ClientExceptionTriggeredWhenClientNull()
+        {
+            //Arrange
+            ClientService clientService = new ClientService();
+
+            /*try
+            {
+                clientService.UpdatePremiumClient(null, "0941924921", "test", "test", "test");
+            }
+            catch (ClientException ex)
+            {
+                Assert.AreEqual("Client is not provided!", ex.Message);
+            }*/
+
+            //Act
+            var ex = Assert.ThrowsException<ClientException>(() =>
+                clientService.UpdatePremiumClient(null, "09123", "test", "test", "test")); 
+            
+            //Assert
+            Assert.AreEqual("Client is not provided!", ex.Message);
+        }
     }
 }
