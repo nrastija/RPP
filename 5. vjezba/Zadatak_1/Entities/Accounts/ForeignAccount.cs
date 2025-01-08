7a660d8b66d154289192667c56099244c54125ee
nrastija <niko.rastija@gmail.com>
2025-01-08 14:57:46 +0100

    Add predefined source project file

---
+++ 5. vjezba/Zadatak_1/Entities/Accounts/ForeignAccount.cs
@@ -0,0 +1,19 @@
+﻿using Entities.Clients;
+using Entities.Exchange;
+
+namespace Entities.Accounts
+{
+    public class ForeignAccount : Account
+    {
+        public ForeignAccount(string iban, double balance, Currency currency, IClient owner) : base(iban, balance, owner)
+        {
+            Currency = currency;
+            Type = AccountType.ForeignAccount;
+        }
+
+        public override string ToString()
+        {
+            return $"IBAN:{IBAN}, Balance:{Balance}, Available Funds: {AvailableFunds}, Valuta:{Currency.Code}";
+        }
+    }
+}
