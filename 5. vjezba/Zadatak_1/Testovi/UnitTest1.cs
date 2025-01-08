17d2401572fa82231246cd0f7772e9d9b6a63dd7
nrastija <niko.rastija@gmail.com>
2025-01-08 16:20:00 +0100

    Create unit test for manipulating with accounts and testing account balance money deposit

--- 5. vjezba/Zadatak_1/Testovi/UnitTest1.cs
+++ 5. vjezba/Zadatak_1/Testovi/UnitTest1.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Runtime.InteropServices;
+using Entities.Accounts;
+using Entities.Clients;
 using Entities.Exchange;
 
 namespace Testovi
@@ -18,6 +20,19 @@ namespace Testovi
             //Assert
             Assert.IsTrue(currency.Code == "CZK");
             Assert.IsTrue(currency.Country == "Češka");
+        }
+
+        [TestMethod]
+        public void ForeignAccount_Deposit_ValueIsAdded()
+        {
+            //Arrange
+            ForeignAccount account = new ForeignAccount("HR99999999", 1000, null, null);
+
+            //Act
+            account.Deposit(250);
+
+            //Assert
+            Assert.AreEqual(1250, account.Balance, "Nije dobar iznos");
         }
 
     }
