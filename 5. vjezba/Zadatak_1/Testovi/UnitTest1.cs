817c9dcce0b6acd7efa120a5201d8e2f351775b8
nrastija <niko.rastija@gmail.com>
2025-01-08 16:11:31 +0100

    Add unit test for testing Currency constructor

---
+++ 5. vjezba/Zadatak_1/Testovi/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Runtime.InteropServices;
+using Entities.Exchange;
+
+namespace Testovi
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Currrency_Constructor_CodeCountryIsCorrect()
+        {
+            //Arrange
+            Currency currency = new Currency("CZK", "Češka");
+
+
+            //Assert
+            Assert.IsTrue(currency.Code == "CZK");
+            Assert.IsTrue(currency.Country == "Češka");
+        }
+
+    }
+}
