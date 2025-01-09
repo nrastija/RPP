44fa984cf235c57243a48a7a0e5f228a0c3db85a
nrastija <niko.rastija@gmail.com>
2025-01-09 08:25:53 +0100

    Create unit test for testing money withdrawal from foreign account

--- 5. vjezba/Zadatak 3/Tests/UnitTest1.cs
+++ 5. vjezba/Zadatak 3/Tests/UnitTest1.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using Entities.Accounts;
 using Entities.Exchange;
 
 namespace Tests
@@ -16,6 +17,20 @@ namespace Tests
             Assert.AreEqual("HRK", rate.Currency.Code);
             Assert.AreEqual("Croatia", rate.Currency.Country);
             Assert.AreEqual(21, rate.Rate);
+        }
+
+        [TestMethod]
+        public void ForeignAccount_Balance_WithdrawReducesBalance()
+        {
+            //Arrange
+            Currency currencyHRK = new Currency("HRK", "Croatia");
+            ForeignAccount account = new ForeignAccount("1111", 800, currencyHRK, null);
+
+            //Act
+            account.Withdraw(250);
+
+            //Assert
+            Assert.AreEqual(550, account.Balance);
         }
     }
 }
