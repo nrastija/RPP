8f2c9e623ae227288c40a13cb21981cd0caedee0
nrastija <niko.rastija@gmail.com>
2025-01-09 08:23:50 +0100

    Implement Unit test for testing constructor of class ExchangeRate

---
+++ 5. vjezba/Zadatak 3/Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("33d454b1-128e-4781-aa0e-328e73235b0e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ 5. vjezba/Zadatak 3/Tests/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Entities.Exchange;
+
+namespace Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ExchangeRate_Currency_ConstructorWorks()
+        {
+            Currency currencyHRK = new Currency("HRK", "Croatia");
+            ExchangeRate rate = new ExchangeRate(currencyHRK, 21);
+
+            Assert.AreEqual("HRK", rate.Currency.Code);
+            Assert.AreEqual("Croatia", rate.Currency.Country);
+            Assert.AreEqual(21, rate.Rate);
+        }
+    }
+}
